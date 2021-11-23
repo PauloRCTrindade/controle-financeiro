bc05ad369d39cf29d9aeb1d1a0bd3c6559d303f4
Paulo R C Trindade <Paulinhotrindade121212@gmail.com>
2021-11-23 13:46:53 -0300

    init

---
+++ controlefinanceiro/src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "../src/style/global"
+import { Header } from "./components/Header";
+
+export function App() {
+  return (
+    <>
+			<GlobalStyle/>
+      <Header />
+    </>
+  );
+}
---
+++ controlefinanceiro/src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+export function Header(){
+	return (
+		<header>
+			<img src="" alt="controle financeiro" />
+			<button>Nova Transação</button>
+		</header>
+	)
+}

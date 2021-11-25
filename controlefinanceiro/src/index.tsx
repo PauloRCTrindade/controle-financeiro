ba443386c0f5873d5223f1b23f9cd887a77d886e
Paulo R C Trindade <Paulinhotrindade121212@gmail.com>
2021-11-25 15:25:21 -0300

    Update index.tsx

--- controlefinanceiro/src/index.tsx
+++ controlefinanceiro/src/index.tsx
@@ -1,23 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+	models: {
+		transaction: Model,
+	},
+
+	seeds(server) {
+	 server.db.loadData({
+		 transactions: [
+			 {
+				 id:1,
+				 title:'freelance website',
+				 type: 'deposit',
+				 categoty:'Dev',
+				 amount: 6000,
+				 createdAt: new Date('2021-02-12 09:00:00')
+			 },
+			 {
+				id:2,
+				title:'freelance website',
+				type: 'deposit',
+				categoty:'Dev',
+				amount: 3000,
+				createdAt: new Date('2021-07-12 09:00:00')
+			},
+			{
+				id:3,
+				title:'freelance website',
+				type: 'withdraw',
+				categoty:'Dev',
+				amount: 5000,
+				createdAt: new Date('2021-01-12 09:00:00')
+			}
+		 ]
+	 })		
+},
+
 	routes() {
 		this.namespace = 'api';
 
 		this.get('/transactions', ()=> {
-			return [
-				{
-					id:1,
-					title: 'Transaction 1',
-					amount: 400,
-					type: 'deposit',
-					category:'Food',
-					createdAt: new Date()
-				}
-			]
+			return this.schema.all('transaction')
+		})
+
+		this.post('/transactions', (schema,request) => {
+			const data = JSON.parse(request.requestBody)
+
+			return schema.create('transactiion',data);
 		})
 	}
 })

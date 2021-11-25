b50543ffd67d4e28055fb91a7ea081fdc7f87b44
Paulo R C Trindade <Paulinhotrindade121212@gmail.com>
2021-11-25 15:25:11 -0300

    Update index.tsx

--- controlefinanceiro/src/components/Header/index.tsx
+++ controlefinanceiro/src/components/Header/index.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-redeclare */
-import { useState } from 'react'
 import  logoImg from '../../assets/logo.svg'
-import Modal from 'react-modal';
 import { Container, Content } from './styles'
 
 interface Header {
